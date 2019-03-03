f441e3c12bed124d8de565e3a723b8a84bf2ea03
vhammer-tgm <31884902+vhammer-tgm@users.noreply.github.com>
2019-03-03 18:48:15 +0100

    Neuer Artikel: Grobe Geschichte der EU

---
+++ subpages/contentfiles/Grobe Geschichte der EU/Content.php
@@ -0,0 +1,79 @@
+
+        <!-- Title -->
+        <h1 class="mt-4">Grobe Geschichte der EU</h1>
+
+        <!-- Author -->
+        <p class="lead">
+          von
+          <a href="#">Viktoria Hammer</a>
+        </p>
+
+        <hr>
+
+        <!-- Date/Time -->
+        <p>Gepostet am 03.03.2019 um 18:30</p>
+
+        <hr>
+
+        <!-- Preview Image -->
+        <img class="img-fluid rounded" src="eu-geschichte-preview.jpg" alt="">
+
+        <hr>
+
+        <!-- Post Content -->
+        <p class="lead">Die EU. Ein Zusammenschluss von (noch) 28 Ländern, die gemeinsam Regierung, Wirtschaft und Sozialregeln aufstellen. Aber wie kam das überhaupt zustande?</p>
+
+        <p>
+		<video width="100%" height="auto" controls>
+		  <source src="eu-geschichte.mp4" type="video/mp4">
+			Your browser does not the video.
+		</video>
+		
+		</p>
+		
+		<p>Nach dem 2. Weltkrieg wollten viele Länder eine sichere Position und schlossen sich somit zusammen, um Frieden zu garantieren.</p>
+		<p>1951: 6 Länder (Belgien, Frankreich, Italien, Luxemburg, Niederlande und Westdeutschland) unterschrieben die Vertrag von Paris (Effekt in 1952) und gründeten damit die Europäischen Gemeinschaft für Kohle und Stahl, um so gemeinsam diese verwalten zu können.</p> 
+		
+		<p>1957: Verträge von Rom 
+		<br />→ Europäische Wirtschaftsgemeinschaft (EWG) - der Gemeinsame Markt: Abschaffung von Zöllen zwischen EU-Staaten
+		<br />→ Europäische Atomgemeinschaft (Euratom): Zusammenarbeit in Atomforschung
+		</p>
+		<p>1. Januar 1973: + Dänemark, Irland und UK (9)</p>
+		<p>1974: Sturz des portugiesischen Salazar-Regimes<br />
+		1975: Tod spanischen Generals Franco <br />
+		→ Zeit der rechtsgerichteten Diktaturen in Europa vorbei</p>
+		<p>1979: erste direkte Wahl ihrer Vertreter im Parlament</p>
+		<p>1970ern: Vorschriften zum Schutz der Umwelt + Verursacherprinzip</p>
+		<p>1981: + Griechenland (10)</p>
+		<p>1986 + Spanien und Portugal (12)</p>
+		<p>1986: Einheitliche Europäische Akte unterzeichnet → 6 Jahres Programm → Binnenmarkt</p>
+		<p>1989: Fall der Berliner Mauer → politische Wende</p>
+		<p>1990: Wiedervereinigung Deutschlands  </p>
+
+		<p>1993: Binnenmarkt = Freier Verkehr von Waren, Dienstleistungen, Personen und Kapital</p>
+
+		<p>1993: Vertrag von Maastricht — zur Verbesserung der politischen und wirtschaftlichen Integration (→ Euro) + gemeinsame Außen- und Sicherheitspolitik und Zusammenarbeit in Justiz und Inneres. </p>
+		<p>1995: + Österreich, Finnland und Schweden (15)</p>
+		<p>Übereinkommen von Schengen → Grenzen ohne Passkontrolle überqueren → Studieren mit Unterstützung der EU in anderen Ländern</p>
+		<p>1999: Vertrag von Amsterdam (in Kraft tretung): Revision von Maastrichter Vertrag </p>
+
+		<p>2002: Euro als Bargeld in Verwendung (heute zahlen 19 Mitgliedsstaaten damit)</p>
+
+		<p>2004: + 10 neue Staaten (25): Estland, Lettland, Litauen, Polen, Tschechische Republik, Slowakei, Slowenien, Ungarn, Malta, Zypern</p>
+
+
+		<p>2007: + Bulgarien und Rumänien (27)</p>
+		<p>2009: Vertrag von Lissabon tritt ein → moderne Institutionen und effizientere Arbeitsverfahren</p>
+		<p>2012: Friedensnobelpreis</p>
+		<p>2013: + Kroatien (28)</p>
+		<p>2014: Europawahlen → mehr skeptische ins Parlament</p>
+
+		<p><br/>Die Geschichte der EU sollte hier nicht enden.</p>
+
+		<p>
+		<b>Quellen:</b>
+		<a href="https://europa.eu/european-union/about-eu/history_de">Offizielle EU-Seite</a>
+		<a href="https://www.bmi.gv.at/509/Geschichte/start.aspx">BMI</a>
+		</p>
+
+        <hr>
