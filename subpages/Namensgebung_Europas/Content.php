f4cfc09ca87e34a9053277d6afc6bd20f9a4a41e
Ata Icen <ticen@student.tgm.ac.at>
2019-03-12 14:06:58 +0100

    Europa Animation
    Ajde europa dies das

---
+++ subpages/Namensgebung_Europas/Content.php
@@ -0,0 +1,30 @@
+
+        <!-- Title -->
+        <h1 class="mt-4">Namensgebung Europas</h1>
+
+        <!-- Author -->
+        <p class="lead">
+          von
+          <a href="#">Ata Icen</a>
+        </p>
+
+        <hr>
+
+        <!-- Date/Time -->
+        <p>Gepostet am 12. M&auml;rz 2019 um 14:00 Uhr</p>
+
+        <hr>
+
+        <!-- Preview Image -->
+        <video class="responsiv" controls preload="auto">
+    	<source src="EuropaUndZeus.mp4" type="video/mp4">
+        <hr>
+
+        <!-- Post Content -->
+        <p class="lead"></p>
+            
+        <p>Europa und Zeus</p>
+
+        <p>Es wird immer viel &uuml;ber die EU und generell &uuml;ber Eurpa geredet. Aber die mei&szlig;ten wissen nicht einmal wieso wir unser Kontinent so nennen. Diese Animation soll euch ein klareres Bild vor Augen machen.   
+          </p>
+        <hr>
